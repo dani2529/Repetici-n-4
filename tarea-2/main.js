a0bf1764c355df2fa07e56d8f21266823144bf91
Daniela Mendoza <inadmendoza1996@gmail.com>
2024-11-01 18:58:48 +0100

    subo tarea 2

---
+++ tarea-2/main.js
@@ -0,0 +1,25 @@
+let container = document.getElementById('container')
+let boton = document.getElementById('mostrar');
+
+boton.addEventListener('click',mostrarApiData,true)
+
+async function mostrarApiData() {
+
+    const url = 'https://randomuser.me/api';
+    const res = await fetch(url);
+    const data = await res.json();
+    
+    container.innerHTML =`
+                          <img src='${data.results[0].picture.large}'>
+                            <p> celular: ${data.results[0].cell}</p>
+                            <p> email: ${data.results[0].email}</p>
+                            <p> genero: ${data.results[0].gender}</p>                
+                            <p> Nombre: ${data.results[0].name.first}</p>
+                            <p> apellido: ${data.results[0].name.last}</p>
+    
+    `;
+    console.log(data, "data <---- async");
+
+    console.log("entro a la funcnion async");
+
+}
